247b1f19a42422da309a1edd7bcdf0efe1ac210c
kylefarmer85 <kylefarmer85@gmail.com>
2020-09-15 17:01:59 -0500

    finished lab

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -4,7 +4,7 @@
 # or whitespace (' ').
 
 class EmailAddressParser
-    attr_accessor :email
+    # attr_accessor :email
     def initialize(email_addresses)
         @email_addresses = email_addresses
     end
